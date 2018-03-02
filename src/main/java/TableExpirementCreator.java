090b5633c4bb2c800ab0908bd9a260a46c58b2e7
aviad <aviadkeepitforward@gmail.com>
2018-03-02 21:10:19 +0200

    I added a class that provide csv creation.

---
+++ src/main/java/TableExpirementCreator.java
@@ -0,0 +1,12 @@
+public class TableExpirementCreator {
+
+    public TableExpirementCreator(String nameOfcsvFile)
+    {
+
+    }
+
+    public void writeToFile(int x , int y,String content)
+    {
+
+    }
+}
